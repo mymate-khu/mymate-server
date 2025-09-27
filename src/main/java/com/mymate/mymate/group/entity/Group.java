c8463c247375123b31a9a458946e37af99666771
Yoonhojoon <dbsghwns1209@gmail.com>
2025-09-28 03:10:17 +0900

    [HOTFIX] Group 엔티티의 테이블 이름 변경
    - 'groups'에서 'groups_table'로 변경

--- src/main/java/com/mymate/mymate/group/entity/Group.java
+++ src/main/java/com/mymate/mymate/group/entity/Group.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "groups")
+@Table(name = "groups_table")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Group extends BaseEntity {
